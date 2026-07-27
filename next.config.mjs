783f6896cf9e4f438b691b62990af2b1a43255a8
M Hendro Junawarko <muhammadhendro88@gmail.com>
2026-07-27 11:53:51 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -14,10 +14,6 @@ const nextConfig = {
             value: 'max-age=31536000; includeSubDomains'
           },
           {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN'
-          },
-          {
             key: 'X-Content-Type-Options',
             value: 'nosniff'
           },
